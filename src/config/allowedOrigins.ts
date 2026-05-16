a0a6850b5dd045097bc1820a5312aab7658afc7b
ninodev92 <nnejirichard@yahoo.com>
2026-05-16 11:11:58 +0100

    added netlify origin

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://127.0.0.1:5174",
   "https://cool-queijadas-f87675.netlify.app",
+  "https://rainbow-fudge-2bfdbc.netlify.app",
 ];
 
 export default allowedOrigins;
